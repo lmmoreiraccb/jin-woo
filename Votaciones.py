b986a27db54e26b212957c81434b3b6ef9337812
lmmoreiraccb <158303459+lmmoreiraccb@users.noreply.github.com>
2024-01-31 09:18:09 -0600

    versión 1.0

---
+++ Votaciones.py
@@ -0,0 +1,19 @@
+dpi=input("ingresa tu dpi")
+cantidad = len(dpi)
+if cantidad==13:
+    print("dpi valido")
+    print("por quien quieres votar?")
+    voto1=input("Votar por precidente rojo (A) azul (B)")
+    voto2=input("Votar por alcalde rojo (C) azul (D) verde (E) naranja (F)")
+    if voto1 =="A":
+        print("votaste para presidente rojo (A)")
+    elif voto1=="B":
+        print("votaste para presidente azul (B)")
+    if voto2=="C":
+        print("votaste para alcalde rojo (C)")
+    elif voto2=="D":
+        print("votaste para alcalde azul (D)")
+    elif voto2=="E":
+        print("votaste para alcalde verde (E)")
+    elif voto2=="F":
+        print("votaste para alcalde naranja (F)")
