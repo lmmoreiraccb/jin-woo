06689867fd1c4e0c68620b67289f972afd260935
lmmoreiraccb <158303459+lmmoreiraccb@users.noreply.github.com>
2024-02-13 09:18:47 -0600

    Add files via upload

---
+++ Asistencia de Alumnos.py
@@ -0,0 +1,9 @@
+for _ in range(10):
+    carne=input("ingrese numero de carne ")
+    if len(carne) !=8:
+        print("carne invalido ")
+    elif len(carne) ==8:
+        print("carne valido ")
+for nombre in range(10):
+    input("ingrese nombre y apellido ")
+
