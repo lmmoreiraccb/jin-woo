0814da7ba29395cc4f74497251fdc2d7d9d6ca16
lmmoreiraccb <158303459+lmmoreiraccb@users.noreply.github.com>
2024-02-26 11:11:17 -0600

    Add files via upload

--- Ejercicio2tarea1.py
+++ Ejercicio2tarea1.py
@@ -1,8 +1,5 @@
-for i in range(1, 11):
-    print(i)
-    limite = int(input("Ingrese un número límite: "))
+limite = int(input("Ingrese un número límite: "))
 contador = 1
-
 while contador <= limite:
     print(contador)
     contador += 1
