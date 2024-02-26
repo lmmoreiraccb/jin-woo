d1e24aebd4ac2ecaf9166e08f376697975f8773a
lmmoreiraccb <158303459+lmmoreiraccb@users.noreply.github.com>
2024-02-26 11:02:02 -0600

    Add files via upload

---
+++ Ejercicio2tarea1.py
@@ -0,0 +1,8 @@
+for i in range(1, 11):
+    print(i)
+    limite = int(input("Ingrese un número límite: "))
+contador = 1
+
+while contador <= limite:
+    print(contador)
+    contador += 1
---
+++ Ejercicio3tarea1.py
@@ -0,0 +1,7 @@
+cm = float(input("Ingrese una longitud en centímetros: "))
+
+pulgadas = cm * 0.3937007874
+pies = pulgadas / 12
+
+print("La longitud en pulgadas es: ", pulgadas)
+print("La longitud en pies es: ", pies)
