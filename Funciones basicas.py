27196dd9df7b1d51d5ab630b903f75255ebe5055
lmmoreiraccb <158303459+lmmoreiraccb@users.noreply.github.com>
2024-04-17 11:14:37 -0600

    Add files via upload

---
+++ Funciones basicas.py
@@ -0,0 +1,8 @@
+def nombre_de_la_funcion():
+    # Codigo que realiza la tarea
+    pass
+    saludar() #Imprime: Hola, bienvenido a Python
+def saludar(Erwin):
+    print(f"Hola, {Erwin}! Bienvenido a Python.")
+
+saludar("Erwin") #Imprime: Hola, Erwin! Bienvendio a Python. 
